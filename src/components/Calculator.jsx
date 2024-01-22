34fbd12a027056c0c1e945be37bc1e9296778e6d
Varkovi Mark <vmark33@gmail.com>
2024-01-22 13:41:41 +0100

    replaced title, added displaying feature

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,37 +3,49 @@
 import { useState } from "react";
 
 function Calculator({ operators }) {
-  const numbers = [];
+  const clickSoundUrl = "https://www.fesliyanstudios.com/play-mp3/387";
+  const audio = new Audio(clickSoundUrl);
+  const buttons = [];
   operators.forEach((operator) => {
-    numbers.push(operator);
+    buttons.push(operator);
   });
   for (let i = 9; i > -1; i--) {
-    numbers.push(i);
+    buttons.push(i);
   }
+  buttons.push("=");
 
-  const [isButtonClicked, setIsButtonClicked] = useState(false);
-  const [displayNumber, setDisplayNumber] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [displayedValue, setDisplayedValue]  = useState("")
 
-  function displayNum(number) {
-    setIsButtonClicked(true);
-    setDisplayNumber(number)
+  function loadValues(buttonValue) {
+    if (buttonValue === "C") {
+      setInputValue("")
+      setDisplayedValue("")
+    } else if ( buttonValue === "=") {
+       setDisplayedValue(inputValue)
+       setInputValue("")
+    } else {
+      setInputValue((prevValue) => prevValue + buttonValue);
+    }
   }
 
   return (
     <div id="calculator">
-      <div id="monitor"></div>
-      <div id="pre-calc-container">{isButtonClicked && <p>{displayNumber === "C" ? '' : displayNumber }</p>}</div>
+      <div id="monitor">{displayedValue}</div>
+      <div id="pre-calc-container">{<p>{inputValue}</p>}</div>
       <div id="keyboard">
-        {numbers.map((number) => (
+        {buttons.map((buttonValue) => (
           <button
             onClick={() => {
-              displayNum(number)
+              audio.play();
+              loadValues(buttonValue);
+              console.log(inputValue);
             }}
-            id={number}
-            key={number}
-            className={`calc-number ${number === "C" ? "clear-button" : ""}`}
+            id={buttonValue}
+            key={buttonValue}
+            className={`calc-number ${buttonValue === "C" ? "clear-button" : ""}`}
           >
-            {number}
+            {buttonValue}
           </button>
         ))}
       </div>
